20575461ef8e0c6dec92bff075294ea4fef07119
bh3391 <bhakti.pratama.70@gmail.com>
2026-01-07 17:27:33 +0800

    temporary delete blog page

--- src/app/(guest)/packages/page.tsx
+++ src/app/(guest)/packages/page.tsx
@@ -1,5 +1,6 @@
 import PackagesSection from "@/components/section/packages";
 import Hero from "@/components/section/hero"; // Asumsi Anda punya hero section
+import PickupSection from "@/components/section/pick-up";
 
 
 
@@ -8,6 +9,7 @@ export default function PackagesPage() {
     <main>
       <Hero />
       <PackagesSection />
+      <PickupSection />
       {/* Section lainnya... */}
     </main>
   );
